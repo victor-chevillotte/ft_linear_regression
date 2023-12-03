Fix normalization and precision calculation in utils.py

train.py
@@ -6,28 +6,27 @@ import os
 import pandas as pd
 from utils import (
     normalizeLst,
-    normalizeElem,
-    denormalizeElem,
     meanError0,
     meanError1,
     computeCost,
-    computeRMSE,
+    computePrecision
 )
 
 # Paramètres initiaux
 theta0 = 0
 theta1 = 0
-learning_rate = 0.05
+learning_rate = 0.02
 epochs = 5000
 current_epoch = 0
 display_step = 100
 cost_history = []
-rmse_history = []
-training_threshold = 0.000001
+precision_history = []
+training_threshold = 0.00000001
+
 
 # The algorithm : iterating towards local minimum
-def gradientDescent(epochs, learning_rate, normedMileages, normedPrices):
-    global theta0, theta1, current_epoch, cost_history, rmse_history
+def gradientDescent(epochs, learning_rate, normedMileages, normedPrices, df):
+    global theta0, theta1, current_epoch, cost_history, precision_history
     for i in range(epochs):
         current_epoch += 1
         theta0 = theta0 - learning_rate * meanError0(
@@ -37,8 +36,22 @@ def gradientDescent(epochs, learning_rate, normedMileages, normedPrices):
             normedMileages, normedPrices, theta0, theta1
         )
         cost_history.append(computeCost(normedMileages, normedPrices, theta0, theta1))
-        rmse_history.append(computeRMSE(normedMileages, normedPrices, theta0, theta1))
-
+        theta0_denorm = (
+            theta0 * (max(df["price"]) - min(df["price"]))
+            + min(df["price"])
+            - theta1
+            * min(df["mileage"])
+            * (max(df["price"]) - min(df["price"]))
+            / (max(df["mileage"]) - min(df["mileage"]))
+        )
+        theta1_denorm = (
+            theta1
+            * (max(df["price"]) - min(df["price"]))
+            / (max(df["mileage"]) - min(df["mileage"]))
+        )
+        precision = computePrecision(df["mileage"], df["price"], theta0_denorm, theta1_denorm)
+        
+        precision_history.append(100 - round(precision * 100, 2))
 
 def load_data():
     filename = "data/data.csv"
@@ -51,27 +64,6 @@ def load_data():
     return df
 
 
-def update(frame, axs, df, normedMileages, normedPrices):
-    print("yo")
-    global current_epoch
-    print(f"Epoch {current_epoch}")
-
-    # Nettoyage des graphiques
-    axs[0, 0].cla()
-    axs[0, 1].cla()
-
-
-    # Mise à jour du graphique classique
-    axs[0, 0].scatter(df["mileage"], df["price"])
-    axs[0, 0].set_xlabel("Mileage")
-    axs[0, 0].set_ylabel("Price")
-    axs[0, 0].set_title(f"Price of Cars Epoch {current_epoch}")
-
-    # Calculer la ligne de prédiction
-    lineX = np.array([min(df["mileage"]), max(df["mileage"])])
-    lineY = theta0 + theta1 * lineX
-    axs[0, 0].plot(lineX, lineY, color="red")
-
 def update_graphs(axs, df, normedMileages, normedPrices):
     global theta0, theta1, current_epoch
     print(f"Epoch {current_epoch}")
@@ -80,11 +72,11 @@ def update_graphs(axs, df, normedMileages, normedPrices):
     classicPlot = axs[0, 0]
     normedPlot = axs[0, 1]
     costPlot = axs[1, 0]
-    rmsePlot = axs[1, 1]
+    precisionPlot = axs[1, 1]
     classicPlot.cla()
     normedPlot.cla()
     costPlot.cla()
-    rmsePlot.cla()
+    precisionPlot.cla()
     normedPlot.set_title(f"Price of cars (normed) Epoch {current_epoch}")
     normedPlot.set_xlabel("Mileage")
     normedPlot.set_ylabel("Price")
@@ -94,20 +86,34 @@ def update_graphs(axs, df, normedMileages, normedPrices):
     classicPlot.scatter(df["mileage"], df["price"])
     normedPlot.scatter(normedMileages, normedPrices)
 
+    # Denormalize theta values
+    # TODO : explicit formula for denormalization
 
-    # Calculate linear equation not normalized
-    lineX = [float(min(df["mileage"])), float(max(df["mileage"]))]
-    min_normed_mileage = min(normedMileages)
-    max_normed_mileage = max(normedMileages)
-    lineY = []
-    for elem in lineX:
-        elem = theta1 * normalizeElem(df["mileage"], elem) + theta0
-        lineY.append(denormalizeElem(df["price"], elem))
-    classicPlot.plot(
-        lineX,
-        lineY,
-        color="red",
+    theta0_denorm = (
+        theta0 * (max(df["price"]) - min(df["price"]))
+        + min(df["price"])
+        - theta1
+        * min(df["mileage"])
+        * (max(df["price"]) - min(df["price"]))
+        / (max(df["mileage"]) - min(df["mileage"]))
     )
+    theta1_denorm = (
+        theta1
+        * (max(df["price"]) - min(df["price"]))
+        / (max(df["mileage"]) - min(df["mileage"]))
+    )
+    classicPlot.plot(
+        [min(df["mileage"]), max(df["mileage"])],
+        [
+            theta0_denorm + theta1_denorm * min(df["mileage"]),
+            theta1_denorm * max(df["mileage"]) + theta0_denorm,
+        ],
+        color="C1",
+        label="f(x) = {0}*x + {1}".format(
+            round(theta1_denorm, 4), round(theta0_denorm, 4)
+        ),
+    )
+    classicPlot.legend()
 
     # Normed plot
     min_normed_mileage = min(normedMileages)
@@ -119,54 +125,63 @@ def update_graphs(axs, df, normedMileages, normedPrices):
             theta0 + theta1 * max_normed_mileage,
         ],
         color="red",
+        label="f(x) = {0}*x + {1}".format(round(theta0, 4), round(theta1, 4)),
     )
+    normedPlot.legend()
     costPlot.set_xlabel("Epoch")
     costPlot.set_ylabel("Cost")
     costPlot.set_title("Cost Function Over Time")
     costPlot.plot(range(current_epoch), cost_history[:current_epoch], color="blue")
 
-    rmsePlot.set_xlabel("Epoch")
-    rmsePlot.set_ylabel("RMSE")
-    rmsePlot.set_title("RMSE Over Time")
-    rmsePlot.plot(range(current_epoch), rmse_history[:current_epoch], color="green")
+    precisionPlot.set_xlabel("Epoch")
+    precisionPlot.set_ylabel("Precision %")
+    precisionPlot.set_title("Precision Over Time")
+    precisionPlot.plot(range(current_epoch), precision_history[:current_epoch], color="green")
+
 
 def train(axs, df, normedMileages, normedPrices):
     for epoch in range(int(epochs / display_step)):
-        if (len(cost_history) > 1 and cost_history[-1] > cost_history[-2] - training_threshold):
+        if (
+            len(cost_history) > 1
+            and cost_history[-1] > cost_history[-2] - training_threshold
+        ):
             print("Cost is not decreasing anymore, stopping training")
             break
-        gradientDescent(display_step, learning_rate, normedMileages, normedPrices)
+        gradientDescent(display_step, learning_rate, normedMileages, normedPrices, df)
         update_graphs(axs, df, normedMileages, normedPrices)
-        plt.gcf().canvas.draw_idle()   # Redessiner le graphique
+        plt.gcf().canvas.draw_idle()  # Redessiner le graphique
         plt.gcf().canvas.flush_events()  # Traiter les événements de l'interface utilisateur
 
-def show_results(df): 
 
-    # Calcul et affichage du RMSE final et du pourcentage
-    final_rmse = rmse_history[-1]
-
-    def RMSE_percent(cost):
-        RMSE = 100 * (1 - cost**0.5)
-        return RMSE
-
-    def MSE_percent(cost):
-        MSE = 100 * (1 - cost)
-        return MSE
+def show_results(df):
+    global theta0, theta1, cost_history, precision_history
 
     print("theta finaux normalisés")
     print(theta0)
     print(theta1)
-    print("theta finaux")
-    print(denormalizeElem(df["price"], theta0))
-    print(denormalizeElem(df["price"], theta1))
+    print("thetas finaux")
+    theta0_denorm = (
+        theta0 * (max(df["price"]) - min(df["price"]))
+        + min(df["price"])
+        - theta1
+        * min(df["mileage"])
+        * (max(df["price"]) - min(df["price"]))
+        / (max(df["mileage"]) - min(df["mileage"]))
+    )
+    theta1_denorm = (
+        theta1
+        * (max(df["price"]) - min(df["price"]))
+        / (max(df["mileage"]) - min(df["mileage"]))
+    )
+    print(theta0_denorm)
+    print(theta1_denorm)
     print("cost")
     print(cost_history[-1])
-    print("rmse")
-    print(rmse_history[-1])
-    rmse_percentage = RMSE_percent(cost_history[-1])
-    mse_percentage = MSE_percent(cost_history[-1])
-    print("RMSE en pourcentage : {:.2f}%".format(rmse_percentage))
-    print("MSE en pourcentage : {:.2f}%".format(mse_percentage))
+    error = computePrecision(df["mileage"], df["price"], theta0_denorm, theta1_denorm) 
+
+    # Output precision
+    print("Precision is", 100 - round(error * 100, 2), "% (average error is", round(error * 100, 2), "%")
+    
 
 
 def main():
@@ -176,7 +191,7 @@ def main():
     classicPlot = axs[0, 0]
     normedPlot = axs[0, 1]
     costPlot = axs[1, 0]
-    rmsePlot = axs[1, 1]
+    precisionPlot = axs[1, 1]
     classicPlot.scatter(df["mileage"], df["price"])
     classicPlot.set_xlabel("Mileage")
     classicPlot.set_ylabel("Price")
@@ -195,18 +210,18 @@ def main():
     costPlot.set_title("Cost Function Over Time")
     costPlot.plot(range(current_epoch), cost_history[:current_epoch], color="blue")
 
-    rmsePlot.set_xlabel("Epoch")
-    rmsePlot.set_ylabel("RMSE")
-    rmsePlot.set_title("RMSE Over Time")
+    precisionPlot.set_xlabel("Epoch")
+    precisionPlot.set_ylabel("Precision %")
+    precisionPlot.set_title("Precision Over Time")
     fig.subplots_adjust(hspace=0.3)  # Add space between rows
 
     ax_button = plt.axes([0.05, 0.9, 0.1, 0.075])
-    btn = Button(ax_button, 'Start training')
+    btn = Button(ax_button, "Start training")
 
     def on_button_clicked(event):
-        btn.label.set_text("Training...") 
-        btn.color = 'gray' 
-        btn.hovercolor = 'gray'
+        btn.label.set_text("Training...")
+        btn.color = "gray"
+        btn.hovercolor = "gray"
         btn.active = False
         normedMileages = normalizeLst(df["mileage"])
         normedPrices = normalizeLst(df["price"])
@@ -215,7 +230,6 @@ def main():
 
     btn.on_clicked(on_button_clicked)
     plt.show()
-
 
 
 if __name__ == "__main__":

utils.py
@@ -7,13 +7,6 @@ def normalizeLst(lst):
         normalized.append((value - min(lst)) / (max(lst) - min(lst)))
     return normalized
 
-def	normalizeElem(list, elem):
-	return ((elem - min(list)) / (max(list) - min(list)))
-
-def	denormalizeElem(list, elem):
-	return ((elem * (max(list) - min(list))) + min(list))
-
-
 # Goal function
 def estimatePrice(theta0, theta1, mileage):
     return theta0 + (theta1 * mileage)
@@ -36,9 +29,20 @@ def meanError1(x, y, theta0, theta1):
         adjustedSum += (estimatePrice(theta0, theta1, x[i]) - y[i]) * x[i]
     return adjustedSum / m
 
-def computeRMSE(x, y, theta0, theta1):
-    return np.sqrt(computeCost(x, y, theta0, theta1))
-
+# Compute precision
+def computePrecision (x, y, theta0, theta1):
+	m = len(x)
+	errorSum = 0
+	for i in range(m):
+    	#prevent division by 0
+		if y[i] == 0:
+			continue
+		error = (estimatePrice(theta0, theta1, x[i]) - y[i]) / y[i]
+		if error > 0:
+			errorSum += error
+		else:
+			errorSum -= error
+	return errorSum / m
 
 
 